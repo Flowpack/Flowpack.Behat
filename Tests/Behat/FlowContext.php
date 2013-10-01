78aa6560ff4fc29f68068ca4e09d760162eb4771
Karsten Dambekalns <karsten@typo3.org>
2013-10-01 10:23:03 +0200

    [TASK] Add file docblocks and license to composer manifest
    Change-Id: Id87d2874560bc363a622a90540bb0299a1f2cd4c

--- Tests/Behat/FlowContext.php
+++ Tests/Behat/FlowContext.php
@@ -1,6 +1,16 @@
 <?php
 namespace Flowpack\Behat\Tests\Behat;
 
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Flowpack.Behat".        *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
 use Behat\Behat\Context\BehatContext;
 use Behat\Behat\Event\ScenarioEvent;
 use TYPO3\Flow\Core\Booting\Scripts,
