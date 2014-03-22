027791ba2ace28617edc62745fb2b578191d7bfa
Marc Neuhaus <mneuhaus@famelo.com>
2014-03-22 14:34:28 +0100

    [FEATURE] Warmup cache for Development/Behat context
    This prevents timeouts because of a to long first request

--- Tests/Behat/FlowContext.php
+++ Tests/Behat/FlowContext.php
@@ -73,6 +73,8 @@ class FlowContext extends BehatContext {
 		Scripts::initializePackageManagement($bootstrap);
 		$bootstrap->buildRuntimeSequence()->invoke($bootstrap);
 
+		exec('FLOW_CONTEXT=Development/Behat ./flow flow:cache:warmup');
+
 		return $bootstrap;
 	}
 
