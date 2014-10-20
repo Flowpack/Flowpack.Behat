50d63b2de238dbac97272a3cc311627efb315051
Rens Admiraal <rens@rensadmiraal.nl>
2014-10-20 13:33:22 +0200

    [TASK] Adapt to ACL changes in Flow

--- Tests/Behat/FlowContext.php
+++ Tests/Behat/FlowContext.php
@@ -225,9 +225,6 @@ class FlowContext extends BehatContext {
 	 */
 	protected function resetRolesAndPolicyService() {
 		$this->objectManager->get('TYPO3\Flow\Security\Policy\PolicyService')->reset();
-
-		$roleRepository = $this->objectManager->get('TYPO3\Flow\Security\Policy\RoleRepository');
-		\TYPO3\Flow\Reflection\ObjectAccess::setProperty($roleRepository, 'newRoles', array(), TRUE);
 	}
 
 	/**
